403706bbec53ea628bae78d0f9e7b53c03c1b22e
Emily Welsch <emwelsch@gmail.com>
2018-06-30 17:26:15 +0000

    filling in methods with test data and scrape code

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,29 +1,48 @@
 # require "surfguru/version"
 require 'nokogiri'
-require 'colorize'
+require 'open-uri'
+require 'pry'
 require_relative '../lib/beach.rb'
-require_relative '../lib/cli.rb'
 
 class Scraper
 
   def self.scrape_continents
-    continents = []
-    
+    continents = ["Africa", "Asia", "Europe", "North America", "Oceania", "South America", "Everywhere"]
+    # continents = []
+    #   doc = Nokogiri::HTML(open("https://www.surfline.com/surf-reports-forecasts-cams/"))
+    #   binding.pry
+    #   continent.name = doc.search('a').attr('data-reactid').text.strip
+    #   continents << continent
   end
 
-  def self.scrape_regions
-    regions = []
+  def self.scrape_countries
+    # countries = ["Algeria", "Angola", "Cape Verde", "Ghana", "Ivory Coast", "Liberia", "Madagascar"]
+    countries = []
 
   end
 
   def self.scrape_beaches
+    # beaches = ["Annaba", "Decaplage", "Plage Mandjane", "Pipeline", "Chia"]
     beaches = []
 
   end
 
   def self.scrape_beach_details
     beach_details = {}
-
+    # name
+    # surf height
+    # wind (kts)
+    # swell (ft)
+    # water temp (f)
+    # outside temp (f)
+    # ideal swell direction
+    # ideal wind direction
+    # ideal surf height (ft)
+    # ideal tide
+    # first light
+    # sunrise
+    # sunset
+    # last light
   end
 
 
