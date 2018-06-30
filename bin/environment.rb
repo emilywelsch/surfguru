6b69f1eaa9acd742299b1f8f2ede01e867eba0c6
Emily Welsch <emwelsch@gmail.com>
2018-06-30 15:29:49 +0000

    created environment requirements

---
+++ bin/environment.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'require_all'
+require 'colorize'
+
+require_relative '../lib/surfguru/version.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/beach.rb'
+require_relative '../lib/scraper.rb'
