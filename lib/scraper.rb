effeb3163696a66a967eb9175ae21f7225a76dce
Emily Welsch <emwelsch@gmail.com>
2018-06-30 16:35:57 +0000

    scraper methods outlined

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,25 @@ require_relative '../lib/cli.rb'
 
 class Scraper
 
+  def self.scrape_continents
+    continents = []
+    
+  end
+
+  def self.scrape_regions
+    regions = []
+
+  end
+
+  def self.scrape_beaches
+    beaches = []
+
+  end
+
+  def self.scrape_beach_details
+    beach_details = {}
+
+  end
 
 
 end
