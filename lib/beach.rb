3ebfc10d3139606f8c6e7f5232a2d5381a3b91b9
Emily Welsch <emwelsch@gmail.com>
2018-07-09 20:48:18 +0000

    updated attr_accessors

--- lib/beach.rb
+++ lib/beach.rb
@@ -1,7 +1,7 @@
 # require "surfguru/version"
 
 class Beach
-  attr_accessor :name, :surf_height, :tide_height, :wind, :country_name, :country_url, :beach_url, :continent_name
+  attr_accessor :name, :surf_height, :tide_height, :wind, :url, :country, :continent
   @@all = []
 
   def initialize(attributes)
