01aa67ee6d42ff12eaddf584fe63748160b8e006
Emily Welsch <emwelsch@gmail.com>
2018-07-09 20:46:54 +0000

    created continent class

---
+++ lib/continent.rb
@@ -0,0 +1,33 @@
+# require "surfguru/version"
+
+class Continent
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_country(country)
+    country.continent = self
+  end
+
+  def add_country_by_name(name)
+    country = Country.new(name)
+    country.continent = self
+  end
+
+  def countries
+    Country.all.select {|country| country.continent == self}
+  end
+
+  def self.country_count
+    Country.all.count
+  end
+
+end
