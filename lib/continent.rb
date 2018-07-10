142f1dda55c551939d5c0e0062d34cbc57642607
Emily Welsch <emwelsch@gmail.com>
2018-07-10 18:25:45 +0000

    refractoring redundancies

--- lib/continent.rb
+++ lib/continent.rb
@@ -13,21 +13,21 @@ class Continent
     @@all
   end
 
-  def add_country(country)
-    country.continent = self
-  end
-
-  def add_country_by_name(name)
-    country = Country.new(name)
-    country.continent = self
-  end
-
-  def countries
-    Country.all.select {|country| country.continent == self}
-  end
-
-  def self.country_count
-    Country.all.count
-  end
+  # def add_country(country)
+  #   country.continent = self
+  # end
+  #
+  # def add_country_by_name(name)
+  #   country = Country.new(name)
+  #   country.continent = self
+  # end
+  #
+  # def countries
+  #   Country.all.select {|country| country.continent == self}
+  # end
+  #
+  # def self.country_count
+  #   Country.all.count
+  # end
 
 end
