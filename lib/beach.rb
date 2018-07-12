09e628698a22c2a7485bf65e3ef1ecd83f2b6581
Emily Welsch <emwelsch@gmail.com>
2018-07-12 17:14:22 +0000

    added addtional attr_accessors

--- lib/beach.rb
+++ lib/beach.rb
@@ -1,25 +1,31 @@
 # require "surfguru/version"
 
 class Beach
-  attr_accessor :name, :url, :surf_height, :tide_height, :wind, :country, :continent
+  attr_accessor :name, :url, :surf_height, :tide_height, :wind, :water_temp, :outside_temp, :first_light, :sunrise, :sunset, :last_light, :country, :continent
   @@all = []
 
-  def initialize(attributes)
-    attributes.each {|k, v| self.send(("#{k}="), v)}
-    @@all << self unless @@all.any? {|beach| beach.url == self.url}
+  def initialize(beach_hash)
+    beach_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
   end
 
-  def self.create_new(beach_array)
-    beach_array.each do |beach_hash|
-      self.new(beach_hash)
-    end
+  def self.clear_all
+    @@all = []
   end
 
-  def add_attributes(details_hash)
-    details_hash.each do |k, v|
-      self.send(("#{k}="), v)
-    end
-  end
+  # def self.create_new(beach_array)
+  #   beach_array.each do |beach_hash|
+  #     self.new(beach_hash)
+  #   end
+  # end
+  #
+  # def add_attributes(details_hash)
+  #   details_hash.each do |k, v|
+  #     self.send(("#{k}="), v)
+  #   end
+  # end
 
   def self.all
     @@all
